9f88026785d6702a001a3a75abb3e5e60193a1fc
Jamie Pace <jamiejpace@gmail.com>
2021-05-17 20:57:03 -0600

    Add updated variable file

---
+++ model-1.rb
@@ -0,0 +1,19 @@
+name = "Huxley"
+owner = "Jamie"
+breed = "Australian Sheperd"
+age = 2
+paws = 4
+dog_years = 14
+is_good_boy = true
+is_slow = false
+jumps_high = true
+tricks = ["sit", "shake", "lay down"]
+snack_time = [8, 12, 5, 9]
+commands = Hash.new
+commands = {"point" => "go to bed", "fist" => "sit", "open hand" => "wait"}
+keys = commands.keys
+values = commands.values
+puts "#{keys}"
+puts "#{values}"
+characteristics = Hash.new
+characteristics = {"fur color" => "black and white", "age" => 2, "teeth" => "sharp", "energetic" => true}
